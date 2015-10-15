d9cc14260881a435402d19b4d157d1303d15430d
Damien Maillard <dmaillard06@gmail.com>
2015-10-15 16:54:23 +0200

    prepare event source tests

---
+++ test/1-event-source-connect.js
@@ -0,0 +1,15 @@
+import EventSource from '../lib/event-source.js';
+
+export function beforeAll(){
+	// add an event-source service
+}
+
+export function afterAll(){
+	// remove the event-source service
+}
+
+export function suite(add){
+	add('', function(test){
+
+	});
+}
---
+++ test/3-event-source-idle.js
@@ -0,0 +1,5 @@
+import EventSource from '../lib/event-source.js';
+
+export function suite(add){
+
+}
